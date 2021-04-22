94683cd62c3e535430b51899537a9187e22e05c4
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-04-23 01:48:17 +0530

    Print word 5 times

---
+++ P2.c
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Function Name :  dispaly()
+// Description   :  Print MARVELOUS 5 Time
+// Input         :  -------
+// Output        :  String
+// Author        :  Shivaji das
+// Date          :  23 april 2021
+//
+////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void display()
+{
+	int i=0;
+	for(i=1;i<=5;i++)
+	{
+		printf("MARVELOUS\n");
+	}
+}
+
+int main()
+{
+	display();
+
+	return 0;
+}
