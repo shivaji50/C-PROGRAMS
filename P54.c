b902c12dbb2d43e5204de9cc533bbd784c2db614
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-05-06 23:20:58 +0530

    display pattern

---
+++ P54.c
@@ -0,0 +1,54 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : pattern()
+// Description   : display pattern
+// Input         : iRow = 4    iCol = 4
+// Output        : * * * * 
+//                 * * * # 
+//                 * * # # 
+//                 * # # #
+// Author        : Shivaji das
+// Date          : 6 May 2021
+//
+///////////////////////////////////////////////////
+#include<stdio.h>
+void pattern(int row,int col)
+{
+	int i=0,j=0,no=0;
+	no=col+1;
+	 
+	
+
+	for(i=1;i<=row;i++)
+	{
+		for (j=1;j<=col;j++)
+		{	
+	
+		if(i+j<=no)	
+	    {
+			printf("*\t");
+			
+		}
+		else
+		{
+			printf("#\t");
+		}
+	}
+		
+		
+	      
+		printf("\n");
+	}
+
+}
+
+int main()
+{
+	int value1=0;int value2=0;
+
+	printf("Enter Row and Colum\n");
+	scanf("%d  %d",&value1,&value2);
+	 pattern(value1,value2);
+
+	 return 0;
+}
