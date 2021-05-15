6506f4b676766638c78e939ba4e5e1519a824e93
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-05-15 23:25:41 +0530

    Accept N numbers from user and return frequency of even numbers.

---
+++ P73.c
@@ -0,0 +1,60 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : Dif()
+// Description   : return frequency of even numbers
+// Input         : N : 6 
+//                 Elements : 85 66 3 80 93 88
+// Output        : 3                  
+// Author        : Shivaji das
+// Date          : 15 May 2021
+////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<stdlib.h>
+int Dif(int Arr[],int Length)
+{
+	int i=0,icnt=0;
+	
+
+	for(i=0;i<Length;i++)
+	{	
+		if(Arr[i]%2==0)
+		{
+			icnt++;
+		}
+	}
+	
+		return icnt;
+} 
+
+
+int main()
+{
+    int Noe=0,i=0,Ret=0;
+    int *arr=NULL;
+
+    printf("Enter Number of elemnts");
+    scanf("%d",&Noe);
+
+    arr=(int *)malloc(Noe*sizeof(int));
+    if(arr==NULL)
+    {
+    	printf("Unable to allocate Memory");
+    	return -1;
+    }
+
+    printf("Enter %d Elements\n",Noe);
+
+    for(i=0;i<Noe;i++)
+    {
+    	scanf("%d",&arr[i]);
+    }
+
+    Ret=Dif(arr,Noe);
+
+    printf("The frequency is %d",Ret);
+
+    free(arr);
+
+    return 0;
+
+}
