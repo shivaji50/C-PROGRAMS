b3872cdec02e0b6cfd04e46aa789038d690d9803
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-05-25 17:59:04 +0530

    program which checks whether first and last bit is On or  OFF First bit means bit number 1 and last bit means bit number 32

---
+++ 122.c
@@ -0,0 +1,47 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : CheckBit
+// Description   : checks whether first and last bit is On or OFF
+// Input         : INTEGER
+// Output        : TRUE/FALSE
+// Author        : Shivaji das
+// Date          : 25 May 2021
+////////////////////////////////////////////////////
+#include<stdio.h>
+#include<stdbool.h>
+bool CheckBit(unsigned int iNo)
+{
+    unsigned int iMask=0x80000001;
+    unsigned int iResult=0;
+    
+    iResult=iNo & iMask;
+    if(iResult==iMask)
+    {
+    	return true;
+    } 
+    else
+    {
+    	return false;
+    }
+}
+
+int main()
+{
+	unsigned int iValue=0;
+	bool bret;
+
+	printf("Enter number\n");
+	scanf("%d",&iValue);
+	bret=CheckBit(iValue);
+
+	if(bret==true)
+	{
+		printf("1 and 32 bit is on\n");
+	}
+	else
+	{
+		printf(" 1 and 32 bit is off\n");
+	}
+
+	return 0;
+}
