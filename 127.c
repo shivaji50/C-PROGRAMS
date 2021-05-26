e527f0b5964d29779de1fdb4ce3827de4e2897b5
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-05-26 23:40:35 +0530

    program which accept one number from user and on its first 4  bits. Return modified number.

---
+++ 127.c
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : CheckBit
+// Description   : on its first 4 bits. Return modified number. 
+// Input         : 73
+// Output        : 79
+// Author        : Shivaji das
+// Date          : 26 May 2021
+////////////////////////////////////////////////////
+#include<stdio.h>
+#include<stdbool.h>
+unsigned int CheckBit(unsigned int iNo)
+{
+    unsigned int iMask=0x0000000F;
+    unsigned int iResult=0;    
+    iResult=iNo|iMask;
+    return iResult;
+}
+
+int main()
+{
+	unsigned int iValue=0,bret=0;
+
+	printf("Enter number\n");
+	scanf("%d",&iValue);
+	
+	bret=CheckBit(iValue);
+
+	printf("updated number is :%u\n",bret);
+
+	
+	return 0;
+}
