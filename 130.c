113c35e1e1b6f31a4a76cd334cf5c5a625717831
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-05-27 23:41:41 +0530

    a program which accept one number and position from user and on  that bit. Return modified number.

---
+++ 130.c
@@ -0,0 +1,42 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : CheckBit
+// Description   : on that bit. Return modified number.
+// Input         : 10 3
+// Output        : 14
+// Author        : Shivaji das
+// Date          : 27 May 2021
+////////////////////////////////////////////////////
+#include<stdio.h>
+#include<stdbool.h>
+unsigned int CheckBit(unsigned int iNo,int iPos)
+{
+    unsigned int iMask=0x00000001;
+    unsigned int iResult=0;
+    if((iPos<1)||(iPos>32))
+    {
+    	return false;
+    }
+
+    iMask=iMask<<(iPos-1);
+    iResult=iNo|iMask;
+    return iResult;
+}
+
+int main()
+{
+	unsigned int iValue=0,iPos=0,bret=0;
+	
+
+
+	printf("Enter number\n");
+	scanf("%d",&iValue);
+	printf("Enter Position\n");
+	scanf(" %d",&iPos);
+	bret=CheckBit(iValue,iPos);
+
+	printf("updated number is :%u\n",bret);
+
+	
+	return 0;
+}
