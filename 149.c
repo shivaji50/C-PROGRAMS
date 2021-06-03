b46c8792e9e49c3f8bdd4159bdc52450dac1be2c
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-06-03 23:15:31 +0530

    a recursive program which display below pattern.

---
+++ 149.c
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : DisplayR
+// Description   : display pattern
+// Input         : NULL
+// Output        : A B C D E F
+// Author        : Shivaji das
+// Date          : 3 June 2021
+//////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void DisplayR()
+{
+   static char i='A';
+
+   if(i<='F')
+   {
+   	printf("%c\t",i);
+   	i++;
+   	DisplayR();
+   }
+}
+
+int main()
+{
+	DisplayR();
+	printf("\nEnd of main\n");
+	return 0;
+}
